6798839a9f0b009272090776d4371eaa49529df2
Patryk <byszek.patryk@gmail.com>
2022-03-15 20:40:46 +0100

    Added theming

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+import App from "./App"
+
 import { ChakraProvider } from "@chakra-ui/react"
 
-import App from "./App"
+import theme from "./styles/theme"
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
